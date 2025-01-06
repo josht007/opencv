4c528cf196763baaaab6695650d47f9ee7d45b0d
josht007 <148504666+josht007@users.noreply.github.com.>
2025-01-05 22:59:24 -0500

    Putting one part to theanother

--- tutorial1.py
+++ tutorial1.py
@@ -4,9 +4,10 @@ img = cv2.imread('assets/dunl.jfif', 1)
 img = cv2.resize(img, (0, 0), fx=2, fy=2)
 img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
 
-cv2.imwrite('new_img.sjpeg', img)
+cv2.imwrite('new_img.jpeg', img)
 
 cv2.imshow('Dunked on', img)
+
 
 cv2.waitKey(0)
 
